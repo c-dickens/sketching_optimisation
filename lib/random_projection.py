121346121caac7c0e4a1f2c54f6e94bac3964c0d
Charlie Dickens <dickens.c07@gmail.com>
2019-04-09 20:30:11 +0100

    added rp class and tests

---
+++ lib/random_projection.py
@@ -0,0 +1,254 @@
+import numpy as np
+from numba import njit
+from scipy import sparse
+from scipy.sparse import coo_matrix
+import pyfht
+
+# Helper functions
+
+@njit(fastmath=True)
+def fast_countSketch(SA,row,col,data,sign,row_map):
+    N = len(data)
+    for idx in range(N):
+        SA[row_map[row[idx]],col[idx]]+=data[idx]*sign[row[idx]]
+    return SA
+
+def shift_bit_length(x):
+    '''Given int x find next largest power of 2.
+    If x is a power of 2 then x is returned '''
+    return 1<<(x-1).bit_length()
+
+class RandomProjection:
+    '''
+    Class based implementation of different
+    Random projection methods for RNLA.
+
+    Current examples are taken from [1]
+    however there are various sampling methods
+    which could also be implemented.
+
+    [1] - https://researcher.watson.ibm.com/researcher/files/us-dpwoodru/wNow.pdf'''
+
+    def __init__(self,data,proj_dim,
+                 sketch_type,col_sparsity=1):
+        '''
+        data - ndarray of the data (or vector) to sketch
+        proj_dim - int --> Value to sketch down to
+        sketch_type - str determining which sketch to use
+
+        Sketch Type support args:
+        1. gaussian
+        2. srht
+        3. sjlt
+        '''
+        self.data = data
+        self.n, self.d = data.shape
+        self.proj_dim = proj_dim
+        self.sketch_type = sketch_type
+
+        if self.proj_dim > self.n:
+            raise Exception(f'Sketching with projection dimension\
+                             $self.proj_dim > $self.n is not supported')
+
+        ## For SRHT do the bit shift here so not timed in call
+        ## to sketch
+        if self.sketch_type is 'srht':
+            # preprocess data so length is correct and repeat for
+            # the intermediate function after hadamard transform
+            next_power2_data = shift_bit_length(self.n)
+            deficit = next_power2_data - self.n
+            self.data = np.concatenate((self.data,
+                                        np.zeros((deficit,self.d),
+                                                 dtype=self.data.dtype)),
+                                       axis=0)
+            # set the new n for later use although
+            # sampling will only ever be from self.n
+            # as the power of 2 extension only necessary for
+            # the hadamard transform
+            self.new_n = self.data.shape[0]
+
+        ### SPARSE SKETCHES
+        ### Throw an error here is sketch type is sjlt but
+        ### col sparsity is 1
+
+
+        ### For sparse sketches generate hash functions here so
+        # that they aren't timed in the call to sketch.
+        if self.sketch_type is 'sjlt' or 'countSketch':
+            self.coo_data = coo_matrix(data)
+            self.rows = self.coo_data.row
+            self.cols = self.coo_data.col
+            self.vals = self.coo_data.data
+            self.col_sparsity = col_sparsity
+            if self.sketch_type is 'sjlt' and self.col_sparsity == 1:
+                self.col_sparsity = 2
+
+
+            # To make the code more general let's make an array of
+            # random indices so that we can call the same code for
+            # sjlt as countSketch.
+            # we need self.column_sparsity copies of iid rvs
+            # for the row and sign maps so put these in the array.
+            self._row_map = np.zeros((self.col_sparsity,self.n))
+            self._sign_map = np.zeros((self.col_sparsity,self.n))
+
+            if self.col_sparsity == 1:
+                # Generate a single list for :
+                # 1. row_map
+                # 2. sign_map
+                # Generate single array self.proj_dim x self.d
+                # to populate for the sketch
+                self._row_map = np.random.choice(self.proj_dim,
+                                             self.n,
+                                             replace=True)
+                self._sign_map = np.random.choice(2, self.n, replace=True) * 2 - 1
+                self.SA = np.zeros((self.proj_dim,self.d))
+            else:
+                # set the new projection dimension for sjlt
+                # this is because the sjlt is an m x n sketch
+                # composed of s*(m/s) x n shorter countSketches.
+                self.sjlt_proj_dim = self.proj_dim // self.col_sparsity
+
+                # Generate array whose rows are lists for :
+                # 1. row_map
+                # 2. sign_map
+                # Generate single array self.sjlt_proj_dim
+                # to populate for the sketch to which new local
+                # sketches will be added.
+
+                for _ in range(self.col_sparsity):
+                    self._row_map[_,:] = np.random.choice(self.sjlt_proj_dim,
+                                                 self.n,
+                                                 replace=True)
+                    self._sign_map[_,:] = np.random.choice(2, self.n, replace=True) * 2 - 1
+                self._row_map = self._row_map.astype(int)
+                self.SA = np.zeros((self.sjlt_proj_dim,self.d))
+                #print('size of SA ', self.SA.shape)
+                #print('dType of row map ', self._row_map.dtype)
+
+        ## Function dictionary to call later on.
+
+        self.fct_dict = {'gaussian'    : self.GaussianSketch,
+                         'srht'        : self.SRHT,
+                         'countSketch' : self.CountSketch,
+                         'sjlt'        : self.SparseJLT}
+
+
+    def GaussianSketch(self):
+        '''Compute the Gaussian random transform
+        S_ij = G_ij ~ N(0,1) / sqrt(proj_dim)'''
+        S = np.random.randn(self.proj_dim,self.n)
+        S /= np.sqrt(self.proj_dim)
+        return S@self.data
+
+    def SRHT(self):
+        '''
+        Compute the Subsampled Randomized Hadamard
+        Transform (aka Fast Johnson Lindesntrauss Transform).
+
+        Compute:
+        PHDA where
+        - DA is a diagonal matrix whose entries are
+        {±1} chose uar
+        - H (DA) applies the Hadamard transform on DA
+        - P (HDA) uniformly subsamples HDA.
+
+
+        Notes:
+        The pyfht.fht_inplace doesn't seem to work so we
+        need a little bit of working space (to store the
+        FFTed versions of the columns) to do the
+        entire transform'''
+        diag = np.random.choice([1,-1], self.new_n)[:,None]
+        signed_data = diag*self.data
+
+        # the [:,None] syntax is just to add a 2nd dimension
+        # so that the columns after hadamard transform can
+        # be easily appended.
+        # It is slightly quicker to generate lists, call
+        # np.array, then reshape than initialising a zero
+        # array to store the fhted columns
+
+        #Y = np.zeros((self.new_n,self.d))
+        Y = []
+        # perform the in place fht on each column
+        for _col in range(self.d):
+            # Y[:,_col] = pyfht.fht(self.data[:,_col])
+            Y.append(pyfht.fht(self.data[:,_col]))
+        Y = np.array(Y)
+        Y = Y.T
+        #print(type(Y),Y.shape)
+        #print(Y)
+        # number from num_rows_data universe
+        sample = np.sort(np.random.choice(self.n,
+                                  self.proj_dim,
+                                  replace=False))
+
+        S = Y[sample] * (np.sqrt(1/self.proj_dim))
+        return S
+
+    def CountSketch(self):
+        '''Compute the CountSketch transform of the data.
+        This is just the  SJLT but with column sparsity 1.
+        Given its own method to ensure speed is not
+        implicated during later testing.'''
+        return fast_countSketch(self.SA,
+                    self.rows,
+                    self.cols,
+                    self.vals,
+                    self._sign_map,
+                    self._row_map)
+
+    def SparseJLT(self):
+        '''Compute the SparseJLT of the data
+        using the Kane-Nelson construction of
+        concatenated CountSketches.
+
+        1. Generate `s` independent countsketches
+        each of size m/s x n and concatenate them.
+
+        2. Use initial hash functions as decided above in the
+        class definition and then generate new hashes for
+        subsequent countsketch calls.'''
+        local_row_map = self._row_map[0,:]
+        local_sign_map = self._sign_map[0,:]
+        global_summary = fast_countSketch(self.SA,
+                    self.rows,
+                    self.cols,
+                    self.vals,
+                    local_sign_map,
+                    local_row_map)
+        #print('global summary \n', global_summary)
+        for sketch_id in range(1,self.col_sparsity):
+            #print('sketch_id ', sketch_id+1)
+            #print(self.SA)
+            local_row_map = self._row_map[sketch_id,:]
+            local_sign_map = self._sign_map[sketch_id,:]
+            local_summary = fast_countSketch(np.zeros_like(self.SA),
+                    self.rows,
+                    self.cols,
+                    self.vals,
+                    local_sign_map,
+                    local_row_map)
+            global_summary = np.concatenate((global_summary,
+                                             local_summary),axis=0)
+        global_summary *= 1 / np.sqrt(self.col_sparsity)
+        return global_summary
+
+    def sketch(self):
+        '''
+        Perform the transform sketch(A) = SA
+        '''
+        return self.fct_dict[self.sketch_type]()
+
+    def sketch_data_targets(self,targets):
+        '''
+        For classic sketching aka sketch-and-solve
+        we need to sketch the matrix X where X is the
+        matrix which has data|targets appended'''
+        X = np.c_[self.data,targets]
+        S_Ab = RandomProjection(X,self.proj_dim,self.sketch_type).sketch()
+        SA = S_Ab[:,:-1]
+        Sb = S_Ab[:,-1]
+        #print(SA.shape, Sb.shape)
+        return SA, Sb
---
+++ tests/test_random_projections.py
@@ -0,0 +1,115 @@
+import pytest
+import sys
+sys.path.append("..")
+import numpy as np
+from lib.random_projection import RandomProjection as rp
+
+
+@pytest.fixture
+def data_to_test():
+    '''
+    Generates a sample of test data for the tests.
+    This can be replaced by pulling a real dataset.
+
+    To ensure the data-target test passes for the SRHT
+    then length of the input should be a power of 2.
+    '''
+    data = np.random.randn(2**12,50) / np.random.randn(2**12,50)
+    return data
+
+@pytest.fixture
+def all_sketch_methods():
+    '''
+    Returns a list of all sketch methods to test
+    '''
+    return ['gaussian','srht','countSketch','sjlt']
+
+def test_raises_exception_proj_dim_larger_than_n(data_to_test,all_sketch_methods):
+    '''Ensures that the projection dimension is smaller than n'''
+    n,d = data_to_test.shape
+    for sketch_method in all_sketch_methods:
+        with pytest.raises(Exception):
+            print('Testing ', sketch_method)
+            sX = rp(data_to_test,n+1,sketch_method)
+
+def test_summary_method(data_to_test,all_sketch_methods):
+    '''
+    Tests that the correct sketch method
+    will be executed.'''
+    sketch_dim = 100
+    for sketch_method in all_sketch_methods:
+        summary = rp(data_to_test,sketch_dim,sketch_method)
+        assert summary.sketch_type == sketch_method
+
+def test_summary_size(data_to_test,all_sketch_methods):
+    '''
+    Tests that the summary returned has number of rows equal
+    to the required projection dimension'''
+    sketch_dim = 100
+
+    for sketch_method in all_sketch_methods:
+        if sketch_method == 'sjlt':
+            col_sparsity = 2
+        else:
+            col_sparsity = 1
+        summary = rp(data_to_test,sketch_dim,sketch_method, col_sparsity)
+        _sketch = summary.sketch()
+        print('Sketch size is ', _sketch.shape)
+        assert _sketch.shape[0] == sketch_dim
+
+def test_embedding_improves_with_proj_dim(data_to_test,all_sketch_methods):
+    '''
+    Test that error decreases as we increase projection dimension'''
+    n,d = data_to_test.shape
+    sketch_dims = [2*d,5*d,10*d]
+    errors = [0,0,0]
+    trials = 5
+    covariance = data_to_test.T@data_to_test
+
+    for sketch_method in all_sketch_methods:
+        for idx in range(len(sketch_dims)):
+            sketch_dim = sketch_dims[idx]
+            print(idx)
+            error = 0
+            for i in range(trials):
+                summary = rp(data_to_test,sketch_dim,sketch_method)
+                SA = summary.sketch()
+                sketch_covariance = SA.T@SA
+                error += np.linalg.norm(sketch_covariance - covariance,ord='fro')/np.linalg.norm(covariance,ord='fro')
+            errors[idx] = error / trials
+
+        print(errors)
+        assert errors[2] <= errors[1]
+        assert errors[1] <= errors[0]
+
+def test_sketch_data_targets(data_to_test,all_sketch_methods):
+    '''
+    Test that the output is correct dimensionality
+    when the input is the data-target pair (A,b).
+
+    Note that this test will fail when the input has been extended
+    for the SRHT and the same extension has not been applied to y.
+
+    '''
+    n,d = data_to_test.shape
+    y = np.random.randn(n)
+    sketch_dim = 5*d
+    for sketch_method in all_sketch_methods:
+        summary = rp(data_to_test,sketch_dim,sketch_method)
+        SA,Sb = summary.sketch_data_targets(y)
+        assert SA.shape == (sketch_dim,d)
+        assert Sb.shape == (sketch_dim,)
+
+
+
+
+
+
+def main():
+    X = np.random.randn(10_000,50)
+    summary = rp(X,1000,'sjlt')
+    sX = summary.sketch()
+    print('sX shape = ', sX.shape)
+
+if __name__ == '__main__':
+    main()
