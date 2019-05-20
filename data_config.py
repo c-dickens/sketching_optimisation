5c085605b52655ebf2f6e5f47b15a769a447d4ac
Charlie Dickens <dickens.c07@gmail.com>
2019-05-20 16:11:04 +0100

    Moved data config

---
+++ data_config.py
@@ -0,0 +1,15 @@
+# Config file to easily extract various datasets
+import os.path
+import numpy as np
+import scipy.sparse
+from data.get_data.get_datasets import all_datasets
+
+datasets = {}
+num_repeats = 10
+
+for data_name in all_datasets.keys():
+    file_name = all_datasets[data_name]['outputFileName'][3:]
+    datasets[data_name] = {'filepath' : 'data/' + file_name + '.npy',
+                           'repeats'  : num_repeats}
+    #print(file_name)
+#print(datasets)
