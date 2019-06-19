88ea8d431022985626dd1aea2cd9a8b255a78154
Charlie Dickens <dickens.c07@gmail.com>
2019-06-19 21:45:55 +0100

    Updated for lasso

--- lib/synthetic_data_generators.py
+++ lib/synthetic_data_generators.py
@@ -27,3 +27,20 @@ def gaussian_design_unconstrained(nsamples, nfeatures, variance, density=None):#
     noise = np.random.normal(loc=0.0,scale=variance,size=(nsamples,))
     y = A@x_true + noise
     return A, y, x_true
+
+
+def my_lasso_data(m, n, sigma=1.0, density=0.2):
+    '''Generates data matrix X and observations Y.
+    density refers to solution density, not data density'''
+    np.random.seed(1)
+    beta_star = np.random.randn(n)
+    idxs = np.random.choice(range(n), int((1-density)*n), replace=False)
+    for idx in idxs:
+        beta_star[idx] = 0
+    X = np.random.randn(m,n)
+    Y = X.dot(beta_star) + np.random.normal(0, sigma, size=m)
+    scaler = StandardScaler().fit(X)
+    new_X = scaler.transform(X)
+    #scale_y = Normalizer().fit(Y)
+    #new_y = scaler.transform(Y)
+    return new_X, Y, beta_star
--- lib/utils.py
+++ lib/utils.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 import scipy as sp
+from sklearn.linear_model import Lasso
+from time import process_time
 
 # Error metrics
 def mean_square_error(x1, x2):
@@ -11,3 +13,27 @@ def mean_square_error(x1, x2):
 def prediction_error(data,x1,x2):
     '''compute np.sqrt(1/n)*||A(x1-x2)||_2'''
     return (1/data.shape[0])*np.linalg.norm(data@(x1-x2),2)**2
+
+
+### Lasso functions
+def original_lasso_objective(X,y, regulariser,x, penalty=False):
+
+    if penalty:
+        return 0.5*np.linalg.norm(X@x-y,ord=2)**2 + regulariser*np.linalg.norm(x,1)
+    else:
+        return 0.5*np.linalg.norm(X@x-y,ord=2)**2
+
+def sklearn_wrapper(X,y,n,d, regulariser, trials):
+    '''solves the lasso problem in the sklearn sense by dividing out number
+    of rows for normalisation.'''
+
+    clf = Lasso(regulariser)
+    lasso_time = 0
+    for i in range(trials):
+        print("Trial ", i)
+        lasso_start = process_time()
+        lasso = clf.fit(np.sqrt(n)*X,np.sqrt(n)*y)
+        lasso_time += process_time() - lasso_start
+        x_opt = lasso.coef_
+        f_opt = original_lasso_objective(X,y,regulariser,x_opt,penalty=True)
+    return x_opt, f_opt, lasso_time/trials
